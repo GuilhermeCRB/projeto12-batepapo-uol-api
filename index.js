5995a48aa6075109b9a01f03ec4850cf0ce17571
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-04-29 12:28:18 -0300

    feat: saves post request to participants database

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express, {json} from "express";
+import { MongoClient } from "mongodb";
 import cors from "cors";
 import dotenv from "dotenv";
 import chalk from "chalk";
@@ -8,9 +9,28 @@ app.use(cors());
 app.use(json());
 dotenv.config();
 
-console.log(chalk.white.bold.bgGreenBright("\n Application is online \n"));
+// console.log(chalk.green.bold(`Connection with database ${chalk.blue.bold(`${db.s.namespace}`)} stablished! \n`));
+const mongoClient = new MongoClient(process.env.MONGO_URL);
 
+app.post("/participants", async (req,res) => {
+    const {body} = req;
+    console.log("Post request to \"/participants\" received:", body);
+    try{
+        await mongoClient.connect();
+        const db = mongoClient.db("batepapo-uol");
+        const participantsCollection = db.collection("participants");
+        const participant = await participantsCollection.insertOne({
+            ...body,
+            lastStatus: Date.now()
+        });
 
+        res.status(200).send(participant);
+        mongoClient.close();
+    }catch (error) {
+        res.status(500).send(error);
+        mongoClient.close();
+    }
+});
 
-
-app.listen(5000);
+const port = process.env.PORT;
+app.listen(port, () => console.log(chalk.white.bold.bgGreenBright(`\n Application is online, using port ${port}... \n`)));
