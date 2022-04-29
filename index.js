5b9a5d697bf5ed2776fa47a6e206b000ead44969
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-04-29 17:25:19 -0300

    feat: updates user last status

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express, {json} from "express";
+import express, { json } from "express";
 import { MongoClient } from "mongodb";
 import dayjs from "dayjs";
 import cors from "cors";
@@ -13,10 +13,10 @@ dotenv.config();
 // console.log(chalk.green.bold(`Connection with database ${chalk.blue.bold(`${db.s.namespace}`)} stablished! \n`));
 const mongoClient = new MongoClient(process.env.MONGO_URL);
 
-app.post("/participants", async (req,res) => {
-    const {body} = req;
+app.post("/participants", async (req, res) => {
+    const { body } = req;
     console.log("Post request to \"/participants\" received:", body);
-    try{
+    try {
         await mongoClient.connect();
         const db = mongoClient.db("batepapo-uol");
         const participantsCollection = db.collection("participants");
@@ -28,24 +28,24 @@ app.post("/participants", async (req,res) => {
         const messagesCollection = db.collection("messages");
         const now = dayjs();
         const message = await messagesCollection.insertOne({
-            from: 'xxx', 
-            to: 'Todos', 
-            text: 'entra na sala...', 
-            type: 'status', 
+            from: 'xxx',
+            to: 'Todos',
+            text: 'entra na sala...',
+            type: 'status',
             time: now.format("HH:mm:ss")
         });
 
         res.status(201).send(participant);
         mongoClient.close();
-    }catch (error) {
+    } catch (error) {
         res.status(500).send(error);
         mongoClient.close();
     }
 });
 
-app.get("/participants", async (req,res) => {
+app.get("/participants", async (req, res) => {
     console.log("Get request to \"/participants\" received");
-    try{
+    try {
         await mongoClient.connect();
         const db = mongoClient.db("batepapo-uol");
         const participantsCollection = db.collection("participants");
@@ -53,18 +53,18 @@ app.get("/participants", async (req,res) => {
 
         res.status(200).send(participantsList);
         mongoClient.close();
-    }catch (error) {
+    } catch (error) {
         res.status(500).send(error);
         mongoClient.close();
     }
 });
 
-app.post("/messages", async (req,res) => {
-    const {body} = req;
-    const {headers} = req;
+app.post("/messages", async (req, res) => {
+    const { body } = req;
+    const { headers } = req;
     console.log(headers)
-    console.log("Post request to \"/messages\" received:", body);
-    try{
+    console.log("Post request to \"/messages\" received:", req);
+    try {
         await mongoClient.connect();
         const db = mongoClient.db("batepapo-uol");
         const messagesCollection = db.collection("messages");
@@ -77,28 +77,59 @@ app.post("/messages", async (req,res) => {
 
         res.status(201).send(message);
         mongoClient.close();
-    }catch (error) {
+    } catch (error) {
         res.status(500).send(error);
         mongoClient.close();
     }
 });
 
-app.get("/messages", async (req,res) => {
-    const {limit} = req.query;
+app.get("/messages", async (req, res) => {
+    const { limit } = req.query;
     console.log(`Get request to \"/messages/${limit}\" received`);
-    try{
+    try {
         await mongoClient.connect();
         const db = mongoClient.db("batepapo-uol");
         const messagesCollection = db.collection("messages");
-        const messagesList = await messagesCollection.find().limit(parseInt(limit)).sort({time: -1}).toArray();
+        const messagesList = await messagesCollection.find().limit(parseInt(limit)).sort({ time: -1 }).toArray();
 
         res.status(200).send(messagesList.reverse());
         mongoClient.close();
-    }catch (error) {
+    } catch (error) {
         res.status(500).send(error);
         mongoClient.close();
     }
 });
+
+app.post("/status", async (req, res) => {
+    const { headers } = req;
+    console.log(headers.user)
+    console.log("Post request to \"/status\" received:", headers);
+    try {
+        await mongoClient.connect();
+        const db = mongoClient.db("batepapo-uol");
+        const participantsCollection = db.collection("participants");
+        const participantUpdated = await participantsCollection.updateOne(
+            { name: headers.user },
+            {
+                $set: {
+                    lastStatus: new Date.now()
+                }
+            }
+        );
+
+        res.status(200).send(participantUpdated);
+        mongoClient.close();
+    } catch (error) {
+        res.status(500).send(error);
+        mongoClient.close();
+    }
+});
+
+setInterval(udateUsersStatus, 15000);
+
+function udateUsersStatus(){
+    console.log(1)
+}
 
 const port = process.env.PORT;
 app.listen(port, () => console.log(chalk.white.bold.bgGreenBright(`\n Application is online, using port ${port}... \n`)));
